9ec57d96293e5922e1a417aa6f259a43961301a0
willnx <willnx84@gmail.com>
2019-02-18 11:12:28 -0800

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-vlan",
       author="Nicholas Willhite",
       author_email='willnx84@gmail.com',
-      version='2018.12.31',
+      version='2019.02.18',
       packages=find_packages(),
       include_package_data=True,
       package_files={'vlab_switches' : ['app.ini']},
